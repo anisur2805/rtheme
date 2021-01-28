63ad2b9653b7a5cdaecb8aa604d161ddf386d66a
Anisur Rahman <anisur@orangetoolz.com>
2021-01-29 02:22:44 +0600

    add dynamic menu

--- wp-content/themes/rtheme/inc/classes/class-rtheme.php
+++ wp-content/themes/rtheme/inc/classes/class-rtheme.php
@@ -14,6 +14,7 @@ class Rtheme {
 
     protected function __construct() {
         Assets::get_instance();
+        Menus::get_instance();
         $this->setup_hooks();
     }
 
@@ -31,5 +32,38 @@ class Rtheme {
 			'flex-height' => true,
 			'flex-width' => true
 		]);
+		
+		add_theme_support('custom-background', [
+			'default-color' => '#fff',
+			'default-image' => '',
+			'default-repeat' => 'no-repeat'
+		]);
+		
+		add_theme_support('post-thumbnails');
+		
+		add_theme_support('customize-selective-refresh-widgets');
+		
+		add_theme_support('automatic-feed-links');
+		
+		add_theme_support('html5',
+		[
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+			'script',
+			'style'
+		]);
+		
+		add_editor_style();
+		add_theme_support('wp-block-styles');
+		add_theme_support('align-width');
+		
+		global $content_width;
+		if( ! isset( $content_width ) ) {
+			$content_width = 1200;
+		}
+		
 	}
 }
